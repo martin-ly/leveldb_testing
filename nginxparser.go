ea00672fab26f87e8ce079bb745d52ffda90a41d
bahern <benjamin.ahern@gmail.com>
2013-08-28 12:10:51 -0700

    Started moving code around to try and cleanup some things.  Created a Parse method to handle the Nginx log parsing for now via handler type functions.

--- nginxparser.go
+++ nginxparser.go
@@ -1,46 +1,101 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
 	"bufio"
-	"os"
+	"io"
+	"log"
+	"net/url"
+	"regexp"
 	"time"
 )
 
-func main() {
+type RecordEventValues struct {
+	timestamp int64
+	apiKey    string
+	person    string
+	eventName string
+	// TODO: Need property parsing...
+}
 
-	// Setup for reading from stdin.
-	reader := bufio.NewReader(os.Stdin)
+type SetPropertyValues struct {
+	timestamp int64
+	apiKey    string
+	person    string
+	// TODO: Need property parsing...
+}
 
+type AliasUserValues struct {
+	timestamp int64
+	apiKey    string
+	identity1 string
+	identity2 string
+}
+
+type RecordEventHandler func(RecordEventValues)
+
+type SetPropertyHandler func(SetPropertyValues)
+
+type AliasUserHandler func(AliasUserValues)
+
+func Parse(reader *bufio.Reader, recordEvent RecordEventHandler, setProperty SetPropertyHandler, aliasUser AliasUserHandler) {
 	for {
 		// Read lines...
 		line, err := reader.ReadString('\n')
 
-		if err != nil {
-			// Lame breaking based on EOF.
+		if err == io.EOF {
+			// Break on EOF.
 			break
 		}
 
-		// Look for _k=foo and _p=bar and the timestamp from the Nginx log entry.
-		re := regexp.MustCompile("(?P<timestamp>[\\d]+/[a-zA-Z]+/[\\d]+:[\\d]+:[\\d]+:[\\d]+ ['+|-][\\d]+).*_k=(?P<eventid>[0-9A-Za-z]*).*&_p=(?P<productid>[0-9A-Za-z]*)")
+		if err != nil && err != io.EOF {
+			// Fail on other errors.
+			log.Fatal(err)
+		}
 
+		// Regex for the Nginx log.
+		re := regexp.MustCompile("([\\d]+/[a-zA-Z]+/[\\d]+:[\\d]+:[\\d]+:[\\d]+ ['+|-][\\d]+).*GET /(e|s|a)\\?(.*) HTTP/1.1")
 		matched := re.MatchString(line)
 		if matched {
-			// This matches the three parts.
+			// This matches the timestamp, the method and the query string.
 			slice := re.FindStringSubmatch(line)
-		
+
 			timestamp := slice[1]
-			eventid := slice[2]
-			personid := slice[3]
-			
-			// Setup to parse the Common Log Format. (http://en.wikipedia.org/wiki/Common_Log_Format) 
+
+			methodType := slice[2]
+
+			query := slice[3]
+
+			// Setup to parse the Common Log Format. (http://en.wikipedia.org/wiki/Common_Log_Format)
 			const nginxFormat = "02/Jan/2006:15:04:05 -0700"
-			parsedtimestamp, _ := time.Parse(nginxFormat, timestamp)
-			unixtimestamp := parsedtimestamp.Unix()
-			
-			fmt.Printf("{timestamp:%d, eventid:%s, personid:%s}\n", unixtimestamp, eventid, personid)
+			parsedTimestamp, _ := time.Parse(nginxFormat, timestamp)
+			unixTimestamp := parsedTimestamp.Unix()
+
+			values, err := url.ParseQuery(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			switch methodType {
+			case "e":
+				apiKey := values.Get("_k")
+				person := values.Get("_p")
+				eventName := values.Get("_n")
+				// TODO: Need property parsing...
+
+				recordEvent(RecordEventValues{unixTimestamp, apiKey, person, eventName})
+			case "s":
+				apiKey := values.Get("_k")
+				person := values.Get("_p")
+				// TODO: Need property parsing...
+
+				setProperty(SetPropertyValues{unixTimestamp, apiKey, person})
+			case "a":
+				apiKey := values.Get("_k")
+				identity1 := values.Get("_p")
+				identity2 := values.Get("_p")
+
+				aliasUser(AliasUserValues{unixTimestamp, apiKey, identity1, identity2})
+			}
 		}
 	}
-	
 }
