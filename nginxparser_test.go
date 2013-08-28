f4f875c3bb7d297121212c7653fa6e4a8bdfbfd0
bahern <benjamin.ahern@gmail.com>
2013-08-28 14:49:41 -0700

    Test cleanup.

--- nginxparser_test.go
+++ nginxparser_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-var eventtests = []struct {
+var eventTests = []struct {
 	rawevent  string
 	timestamp int64
 	apiKey    string
@@ -15,6 +15,17 @@ var eventtests = []struct {
 }{
 	{"BUCKET_OWNER BUCKET [22/Feb/2010:18:20:02 -0500] 10.251.27.20 REQUESTER REQUEST_ID OPERATION KEY \"GET /e?_k=foobarapikey&_n=Viewed+Course+Description+Page&Course+ID=2&referrer=%2F&_p=foobarperson&Course+Name=Lean+Format+Parsing+%28OLD+VERSION%2C+LINK+TO+NEW+VERSION+ON+HOMEPAGE%29&_t=1266880560 HTTP/1.1\" 200 ERROR_CODE 43 OBJECT_SIZE TOTAL_TIME TURN_AROUND_TIME \"-\" \"-\"\n",
 		1266880802, "foobarapikey", "foobarperson", "Viewed Course Description Page"},
+}
+
+var aliasTests = []struct {
+	rawevent  string
+	timestamp int64
+	apiKey    string
+	identity1 string
+	identity2 string
+}{
+	{"BUCKET_OWNER BUCKET [22/Feb/2010:18:20:02 -0500] 10.251.27.20 REQUESTER REQUEST_ID OPERATION KEY \"GET /a?_k=foobarapikey&_n=foobaralias&_p=foobarperson&_t=1266880560 HTTP/1.1\" 200 ERROR_CODE 43 OBJECT_SIZE TOTAL_TIME TURN_AROUND_TIME \"-\" \"-\"\n",
+		1266880802, "foobarapikey", "foobarperson", "foobaralias"},
 }
 
 func expectedInt(t *testing.T, expected, actual int64, field string) {
@@ -32,8 +43,7 @@ func expectedStr(t *testing.T, expected, actual, field string) {
 }
 
 func TestParseRecordEvents(t *testing.T) {
-
-	for _, test := range eventtests {
+	for _, test := range eventTests {
 		reader := bufio.NewReader(strings.NewReader(test.rawevent))
 		verify := func(values RecordEventValues) {
 			expectedInt(t, values.timestamp, test.timestamp, "timestamp")
@@ -46,31 +56,14 @@ func TestParseRecordEvents(t *testing.T) {
 }
 
 func TestParseAliasUser(t *testing.T) {
-
-	const input = "BUCKET_OWNER BUCKET [22/Feb/2010:18:20:02 -0500] 10.251.27.20 REQUESTER REQUEST_ID OPERATION KEY \"GET /a?_k=foobarapikey&_n=foobaralias&_p=foobarperson&_t=1266880560 HTTP/1.1\" 200 ERROR_CODE 43 OBJECT_SIZE TOTAL_TIME TURN_AROUND_TIME \"-\" \"-\"\n"
-	reader := bufio.NewReader(strings.NewReader(input))
-
-	verify := func(values AliasUserValues) {
-		const timestamp = 1266880802
-		if values.timestamp != timestamp {
-			t.Errorf("Failed parsing timestamp.  Expected %d, found %d\n", timestamp, values.timestamp)
+	for _, test := range aliasTests {
+		reader := bufio.NewReader(strings.NewReader(test.rawevent))
+		verify := func(values RecordEventValues) {
+			expectedInt(t, values.timestamp, test.timestamp, "timestamp")
+			expectedStr(t, values.apiKey, test.apiKey, "apiKey")
+			expectedStr(t, values.identity1, test.identity1, "identity1")
+			expectedStr(t, values.identity2, test.identity2, "identity2")
 		}
-
-		const apiKey = "foobarapikey"
-		if values.apiKey != apiKey {
-			t.Errorf("Failed parsing apiKey.  Expected %s, found %s\n", apiKey, values.apiKey)
-		}
-
-		const identity1 = "foobarperson"
-		if values.identity1 != identity1 {
-			t.Errorf("Failed parsing identity1.  Expected %s, found %s\n", identity1, values.identity1)
-		}
-
-		const identity2 = "foobaralias"
-		if values.identity2 != identity2 {
-			t.Errorf("Failed parsing identity2.  Expected %s, found %s\n", identity2, values.identity2)
-		}
+		Parse(reader, nil, nil, verify)
 	}
-
-	Parse(reader, nil, nil, verify)
 }
